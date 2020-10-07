efb913bc92d2bfa665255e43c882484c35afc062
alvishwakarma <anchal.vishwakarma@skillsoft.com>
2020-10-07 13:34:37 +0530

    url changed

--- functions/test.js
+++ functions/test.js
@@ -10,7 +10,7 @@ exports.handler = async function(event, context, callback){
     
     var config = {
         method: 'post',
-        url: 'https://www.linkedin.com/oauth/v2/accessToken?client_id=86ftg6210chpct&client_secret=TyKsNyHF4fvNXvrp&redirect_uri=https%3A%2F%2F5f7c618be94b242334734e94--kind-neumann-1df8b5.netlify.app%2Ftest&code='+code+'&grant_type=authorization_code',
+        url: 'https://www.linkedin.com/oauth/v2/accessToken?client_id=86ftg6210chpct&client_secret=TyKsNyHF4fvNXvrp&redirect_uri=https%3A%2F%2Fkind-neumann-1df8b5.netlify.app%2F.netlify%2Ffunctions%2Ftest&code='+code+'&grant_type=authorization_code',
         headers: { 
           'Content-Type': 'application/x-www-form-urlencoded', 
           'Cookie': 'lang=v=2&lang=en-us; bcookie="v=2&2bcc5540-f3ea-4c07-8120-ccd1e23fb787"; bscookie="v=1&2020100704232175576941-b6f7-45ca-8edb-29e9a6abb39eAQErL1PiuuEEtBHz5vV03n8O4Th6nHiS"; lissc=1; lidc="b=OB12:g=3219:u=148:i=1602044680:t=1602089858:s=AQHMYKQCbPsxF4mffBnxVOcs_QFJYBPQ"'
